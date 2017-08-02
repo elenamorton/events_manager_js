244b26e34e10f645045b278364b3ae57fdbfc2bd
Elena Morton <elena.uleia@gmail.com>
2017-08-02 20:58:27 +0000

    first test for the place model

---
+++ test/place.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+const expect = chai.expect
+
+//Importing our places model for the unit test
+const Place = require('../src/place')
+
+describe('Place', () => {
+    
+    describe('position on X axis', () => {
+        it('generate x position upto X_MAX', () => {
+            expect(new Place(4, 2).x_position).to.equal(4);
+        })
+    })
+    
+    
+    
+})
