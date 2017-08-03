184083bccd0d8a5357246fa5cf59db44534b8242
Elena Morton <elena.uleia@gmail.com>
2017-08-03 17:24:27 +0000

    adding tests for price and currency, all passing

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -3,11 +3,17 @@ const DEFAULT_CURRENCY = "USD";
 class Ticket {
     
     constructor(quantity, price, currency) {
+        
         this.quantity = quantity;
         this.price = price;
-        this.curency = currency || DEFAULT_CURRENCY;
+        this.currency = currency || DEFAULT_CURRENCY;
+        process.stdout.write(this.currency);
     }
     
+    static get DEFAULT_CURRENCY() {
+        return DEFAULT_CURRENCY;
+    }
+
 
 }
 
--- test/ticket.js
+++ test/ticket.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 //Importing our places model for the unit test
 const Ticket = require('../app/models/ticket');
 let ticket = new Ticket(20, 5);
+let ticketA = new Ticket(1, 25, "GBP");
 
 describe('Ticket', () => {
     
@@ -11,5 +12,18 @@ describe('Ticket', () => {
         it('get the number of tickets', () => {
             expect(ticket.quantity).to.equal(20);
         });
+        
+        it('get the ticket price value', () => {
+            expect(ticket.price).to.equal(5);
+        });
+        
+        it('get the ticket default price currency', () => {
+            expect(ticket.currency).to.equal(Ticket.DEFAULT_CURRENCY);
+        });
+                
+        it('get the ticket price currency', () => {
+            expect(ticketA.currency).to.equal("GBP");
+        });
+        
     });
 })
