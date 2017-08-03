cbc151252acef58a434dee8cd5620c0ac862bd31
Elena Morton <elena.uleia@gmail.com>
2017-08-03 12:57:48 +0000

    added chance randomizer package, and added randomizer to the point constructor, tdd passing

--- app/models/point.js
+++ app/models/point.js
@@ -2,20 +2,23 @@ const X_MIN = -10;
 const X_MAX = 10;
 const Y_MIN = -10;
 const Y_MAX = 10;
+const SEED = 1;
+
+const chance = require('chance').Chance(SEED);
 
 class Point {
     
     constructor(x, y) {
-        this.x_position = x;
-        this.y_position = y;
+        this.x_position = x || this.getRandomPositionX();
+        this.y_position = y || this.getRandomPositionY();
     }
     
     getRandomPositionX() {
-        return Math.floor(Math.random() * (X_MAX - X_MIN + 1)) + X_MIN;
+        return chance.integer({ min: X_MIN, max: X_MAX });
     }
     
     getRandomPositionY() {
-        return Math.floor(Math.random() * (Y_MAX - Y_MIN + 1)) + Y_MIN;
+        return chance.integer({ min: Y_MIN, max: Y_MAX });
     }
 
     
