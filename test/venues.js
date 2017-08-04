7981dd181ff759f1843dbb59fe952e7c5c8ea853
Elena Morton <elena.uleia@gmail.com>
2017-08-04 10:59:20 +0000

    reorg the tests split, added event uniqucity test

--- test/venues.js
+++ test/venues.js
@@ -3,19 +3,34 @@ const expect = chai.expect;
 
 //Importing our places model for the unit test
 const Venues = require('../app/models/venues');
-let venues = new Venues({1: 20}, {2: 15}, {3: 12});
+let venues = new Venues({1: 20, 2: 15, 3: 12});
+let venuesA = new Venues({3: 20, 2: 12, 3: 12});
 
 describe('Venues', () => {
-    
-    it('keeps only one event on a point', () => {
-        expect(venues.getEventId(3)).to.equal(20);
-    });
+    describe('venues have', () => {
+        it('keeps only one event on a point', () => {
+            expect(venues.getEventId(3)).to.equal(12);
+        });
         
-    it('keeps unique point and event pairs', () => {
-        expect(venues.getEventId(3)).to.have.lengthOf(1);
-    });
+        it('keeps unique points', () => {
+            expect(venues.items).to.include.all.keys(1, 2, 3);
+        });
+        
+        it('keeps unique events', () => {
+            let uniqueEvents = [...new Set(venues.getEvents())];
+            expect(uniqueEvents).to.have.lengthOf(3);
+        });
     
-    it('keeps maximum of Point.DISTANCE_MAX entries', () => {
+        it('keeps maximum of Point.DISTANCE_MAX entries', () => {
+        
+        });
         
     });
+    
+    describe('user exceptions', () => {
+        
+        
+        
+    })
+
 })
