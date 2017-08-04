248be3e57ac9775509017656a642477c71ae033b
Elena Morton <elena.uleia@gmail.com>
2017-08-04 09:53:44 +0000

    adding get event id method, and tdd

--- app/models/venues.js
+++ app/models/venues.js
@@ -5,6 +5,19 @@ const MAX_VENUES = Point.DISTANCE_MAX;
 
 class Venues {
     
+    constructor(venuesObj) {
+        this.length = 0;
+        this.items = {};
+        for (let venue in venuesObj) {
+            this.items[venue] = venuesObj[venue];
+            this.length++;
+
+        }
+    }
+    
+    getEventId(key) {
+        return this.items[key];
+    }
     
 }
 
