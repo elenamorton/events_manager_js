6c916258fbd029d469727bbe279551f1c3796b7a
Elena Morton <elena.uleia@gmail.com>
2017-08-04 09:36:25 +0000

    ading the Venues class

---
+++ app/models/venues.js
@@ -0,0 +1,11 @@
+let UserException = require('../helper.js');
+const Point = require('./point');
+
+const MAX_VENUES = Point.DISTANCE_MAX;
+
+class Venues {
+    
+    
+}
+
+module.exports = Venues;
