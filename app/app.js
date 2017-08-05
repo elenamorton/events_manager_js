8ac895101e41c02087db46d66b9e033f71b5767e
Elena Morton <elena.uleia@gmail.com>
2017-08-05 11:15:06 +0000

    trimming the read input and display properly first message as: Closest events to

--- app/app.js
+++ app/app.js
@@ -4,24 +4,29 @@ const Event = require('./models/event');
 const Venues = require('./models/venues');
 
 const UserException = require('./helper.js');
-const MESSAGE = 'Please enter a position (x,y) or <ctrl>D to exit:> ';
+const MESSAGE = 'Please enter a position (x,y) or <ctrl>D to exit:\n';
+const PROMPT = '> ';
 
 let parser = input => {
     return input;
 }
 
 let readInput = () => {
+    let positionString;
+ 
     process.stdin.setEncoding('utf8');
     process.stdout.write(MESSAGE);
+    process.stdout.write(PROMPT);
+    
     process.stdin.on('readable', () => {
 
-        let position = process.stdin.read();
-        if (position !== null) {
-            if (parser(position) !== undefined) {
-            
-                process.stdout.write(`data: ${position}`);
+        positionString = process.stdin.read();
+        
+        if (positionString !== null) {
+            if (parser(positionString.trim()) !== undefined) {
+                process.stdout.write('Closest events to: (' + `${positionString.trim()}` + '\u0029\n');
             }
-            process.stdout.write(MESSAGE);
+            process.stdout.write(PROMPT);
         }
     });
 
