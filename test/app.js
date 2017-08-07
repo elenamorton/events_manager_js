e11f1a3125bad8e61d76f03471738492c66aadb6
Elena Morton <elena.uleia@gmail.com>
2017-08-07 11:18:12 +0000

    adding skeleton feature test file

--- test/app.js
+++ test/app.js
@@ -1,5 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-//Importing our places model for the unit test
+const Point = require('../app/models/point');
+const Ticket = require('../app/models/ticket');
+const Event = require('../app/models/event');
+const Venues = require('../app/models/venues');
+
 const App = require('../app/app');
+
+describe('Ticket', () => {
+    
+})
