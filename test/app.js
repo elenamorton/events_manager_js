3459c5723fd3432b5985442358c130dad55506d5
Elena Morton <elena.uleia@gmail.com>
2017-08-07 12:56:11 +0000

    adding test databases for the feature test and populating these from the application

--- test/app.js
+++ test/app.js
@@ -8,6 +8,21 @@ const Venues = require('../app/models/venues');
 
 const App = require('../app/app');
 
-describe('Ticket', () => {
-    
+let pointsTestDatabase = [];
+let ticketsTestDatabase = [];
+let eventsTestDatabase = [];
+let venuesTestDatabase = new Venues({});
+let currentPoint = new Point(4,2);
+
+describe('App', () => {
+    describe('full scenario with reference point 4,2', () => {
+        it('successfully generates closest points list', () => {
+            App.createPointsDatabase(pointsTestDatabase);
+            App.createTicketsDatabase(ticketsTestDatabase);
+            App.createEventsDatabase(eventsTestDatabase);
+            App.createVenuesDatabase(venuesTestDatabase);
+            
+            
+        });
+    });
 })
